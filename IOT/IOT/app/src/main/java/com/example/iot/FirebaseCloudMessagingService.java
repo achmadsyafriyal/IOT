f64f7179362dae0c96a9ba81383830faaf48042e
rizaldoar <rizaldo.a.r@gmail.com>
2020-07-23 18:57:44 +0700

    Revert "fixing"
    This reverts commit dd27b85b7a4d344c701a1033bcf46f90e8aab4ef.

--- IOT/IOT/app/src/main/java/com/example/iot/FirebaseCloudMessagingService.java
+++ IOT/IOT/app/src/main/java/com/example/iot/FirebaseCloudMessagingService.java
@@ -1,4 +1,54 @@
 package com.example.iot;
 
-public class FirebaseCloudMessagingService {
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+
+
+public class FirebaseCloudMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if(remoteMessage.getData() != null)
+            sendNotification(remoteMessage);
+
+
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        String title = data.get("tittle");
+        String content = data.get("content");
+
+        NotificationManager notificationManager = (NotificationManager) ContextCompat.getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "IOT";
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O) {
+            @SuppressLint("WrongConstant")
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "IOT notification", NotificationManager.IMPORTANCE_MAX);
+
+            notificationChannel.setDescription("Waning Fire Alert");
+            notificationChannel
+        }
+
+
+
+
+
+
+    }
 }
